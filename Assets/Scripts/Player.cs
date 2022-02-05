92eecceea983b439de1f3019edabfa783423bb2e
Gerard Baholli <gerardbaholli@live.it>
2022-02-05 11:09:46 +0100

    Add lesson 88

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+}
