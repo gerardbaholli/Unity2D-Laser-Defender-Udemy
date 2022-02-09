92ba060a93f1de9021a3e0e738e3eff52d9f836d
Gerard Baholli <gerardbaholli@live.it>
2022-02-09 01:50:03 +0100

    Add lessons from 89 to 91

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,57 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed;
+    [SerializeField] float padding;
+    [SerializeField] Laser laserPrefab;
+    [SerializeField] float projectileSpeed;
+    [SerializeField] Transform shootPosition;
 
-    void Update()
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    private void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    private void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Laser projectile = Instantiate(laserPrefab, shootPosition.transform.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            Destroy(projectile, 4f);
+        }
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+
 
 }
